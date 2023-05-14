19f53e8e5e5022e45eb6430a08ab2db2629ccc97
MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>
2023-05-14 13:39:18 +0600

    Add files via upload
    Signed-off-by: MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>

---
+++ 1 Month Preparation Kit/Week 2/Caesar Cipher/Caesar Cipher.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'caesarCipher' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     * 1. STRING s
+     * 2. INTEGER k
+     */
+
+    public static String caesarCipher(String s, int k) {
+        // Write your code here
+        char[] charArr = s.toCharArray();
+        StringBuilder builder = new StringBuilder();
+        for (Character c : charArr) {
+            int ascii = (int) c;
+            if (ascii >= 97 && ascii <= 122) {
+                ascii = ((ascii - 97 + k) % 26) + 97;
+            }
+            if (ascii >= 65 && ascii <= 90) {
+                ascii = ((ascii - 65 + k) % 26) + 65;
+            }
+            builder.append((char) ascii);
+        }
+        return builder.toString();
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String s = bufferedReader.readLine();
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = Result.caesarCipher(s, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ 1 Month Preparation Kit/Week 2/Caesar Cipher/Caesar Cipher.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'caesarCipher' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. INTEGER k
+#
+
+
+def caesarCipher(s, k):
+    # Write your code here
+    return ''.join([chr((ord(c)+k-97) % 26+97)
+                    if c.islower() else chr((ord(c)+k-65) % 26+65)
+                    if (c.isalpha()) else c for c in s])
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = input()
+
+    k = int(input().strip())
+
+    result = caesarCipher(s, k)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
