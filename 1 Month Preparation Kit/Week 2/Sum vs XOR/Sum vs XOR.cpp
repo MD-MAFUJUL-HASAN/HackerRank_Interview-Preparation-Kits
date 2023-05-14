1c8d94dbb3b47af509d4b744add4d678363a9101
MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>
2023-05-14 15:10:34 +0600

    Add files via upload
    Signed-off-by: MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>

---
+++ 1 Month Preparation Kit/Week 2/Sum vs XOR/Sum vs XOR.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'sumXor' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER n as parameter.
+ */
+
+long sumXor(long n) {
+    long counter = 1;
+    while (n > 0) {
+        if ((n & 1) == 0) {
+            counter = counter << 1;
+        }
+        n = n >> 1;
+    }
+    return counter;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    long n = stol(ltrim(rtrim(n_temp)));
+
+    long result = sumXor(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
---
+++ 1 Month Preparation Kit/Week 2/Sum vs XOR/Sum vs XOR.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sumXor' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+
+def sumXor(n):
+    # Write your code here
+    if n == 0:
+        return 1
+    s = str(bin(n))[2:]
+    r = s.count('0')
+    return 2**r
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = sumXor(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
