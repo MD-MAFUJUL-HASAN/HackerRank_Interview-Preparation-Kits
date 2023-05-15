a18748ca9f6d22b6367fba165ef60669d8fdc58f
MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>
2023-05-15 13:39:57 +0600

    Add files via upload
    Signed-off-by: MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>

---
+++ 1 Month Preparation Kit/Week 3/Balanced Brackets/Balanced Brackets.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'isBalanced' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string isBalanced(string s) {
+    int size;
+    while (s.size() > 0) {
+        size = s.size();
+        for (int i = 0; i < size-1; i ++) {
+            if ( (s[i] == '{' && s[i+1] == '}') || 
+                (s[i] == '(' && s[i+1] == ')') ||
+                (s[i] == '[' && s[i+1] == ']') ) {
+                    s.erase(s.begin()+i,s.begin()+i+2);
+            }
+        }
+        if (size == s.size()) {
+            return "NO";
+        }
+    }
+    return "YES";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string s;
+        getline(cin, s);
+
+        string result = isBalanced(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
---
+++ 1 Month Preparation Kit/Week 3/Balanced Brackets/Balanced Brackets.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'isBalanced' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def isBalanced(s):
+    # Write your code here
+    for i in range(len(s)):
+        s= s.replace('{}','')
+        s= s.replace('()','')
+        s = s.replace('[]','')
+    if len(s) == 0:
+        return 'YES'
+    else:
+        return 'NO'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        s = input()
+
+        result = isBalanced(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
