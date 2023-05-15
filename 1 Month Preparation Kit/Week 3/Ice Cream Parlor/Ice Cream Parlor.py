d070137a5be3017916be02f787a79800d3e35b4a
MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>
2023-05-15 13:21:54 +0600

    Add files via upload
    Signed-off-by: MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>

---
+++ 1 Month Preparation Kit/Week 3/Ice Cream Parlor/Ice Cream Parlor.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'icecreamParlor' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER m
+#  2. INTEGER_ARRAY arr
+#
+
+def icecreamParlor(m, arr):
+    # Write your code here
+    for i in range(len(arr)):
+        if (m - arr[i]) in arr and i != arr.index(m - arr[i]):
+            return sorted([i + 1, arr.index(m - arr[i]) + 1])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
